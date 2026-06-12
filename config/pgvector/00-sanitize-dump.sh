d485b5c20478574846a7695d4b2c99c09fea2a0b
qqqxxxnnn0527 <wffeitas@hotmail.com>
2026-06-12 10:47:44 +0800

    debug

--- config/pgvector/00-sanitize-dump.sh
+++ config/pgvector/00-sanitize-dump.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-DUMP_FILE="/docker-entrypoint-initdb.d/init.sql"
+DUMP_FILE="/docker-entrypoint-initdb-src/init.sql"
 
 if [ ! -f "$DUMP_FILE" ]; then
   return 0 2>/dev/null || exit 0
 fi
 
-tmp_file="$(mktemp)"
-
-tr -d '\r' < "$DUMP_FILE" | sed -e '/^\\restrict/d' -e '/^\\unrestrict/d' > "$tmp_file"
-
-mv "$tmp_file" "$DUMP_FILE"
+tr -d '\r' < "$DUMP_FILE" \
+  | sed -e '/^\\restrict/d' -e '/^\\unrestrict/d' \
+  | psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB"
