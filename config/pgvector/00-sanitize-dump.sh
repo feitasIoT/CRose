d6edcd6a848d8d7b54d2042ef3baeae918526364
qqqxxxnnn0527 <wffeitas@hotmail.com>
2026-06-12 10:58:25 +0800

    debug

--- config/pgvector/00-sanitize-dump.sh
+++ config/pgvector/00-sanitize-dump.sh
@@ -7,5 +7,22 @@ if [ ! -f "$DUMP_FILE" ]; then
 fi
 
 tr -d '\r' < "$DUMP_FILE" \
-  | sed -e '/^\\restrict/d' -e '/^\\unrestrict/d' \
+  | awk '
+      BEGIN { skip_comment_on = 0 }
+      /^\\restrict/ { next }
+      /^\\unrestrict/ { next }
+      skip_comment_on {
+        if ($0 ~ /;[[:space:]]*$/) {
+          skip_comment_on = 0
+        }
+        next
+      }
+      /^COMMENT ON / {
+        if ($0 !~ /;[[:space:]]*$/) {
+          skip_comment_on = 1
+        }
+        next
+      }
+      { print }
+    ' \
   | psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB"
